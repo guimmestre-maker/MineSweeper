1696d1a250f684e2361dcdba8f4e13c9db571fd9
guimmestre-maker <guimmestre@gmail.com>
2026-05-14 11:40:31 +0100

    Organização pastas

--- src/main/java/ips/poo/Main.java
+++ src/main/java/ips/poo/Main.java
@@ -7,6 +7,5 @@ import javafx.scene.chart.StackedBarChart;
 public class Main {
     public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro(Dificuldade.FACIL);
-        System.out.printf("%d", tabuleiro.getNumeroBombas());
     }
 }
