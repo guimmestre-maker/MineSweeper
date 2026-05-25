9c14d0e99b15f73579cef301018eec2a8911afcf
guimmestre-maker <guimmestre@gmail.com>
2026-05-25 08:44:10 +0100

    dependencias

--- src/main/java/ips/poo/Main.java
+++ src/main/java/ips/poo/Main.java
@@ -1,12 +1,10 @@
 package ips.poo;
 
-import ips.poo.Model.Dificuldade;
-import ips.poo.Model.Tabuleiro;
+
 import ips.poo.View.Menu;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import static javafx.application.Application.launch;
 
 public class Main extends Application {
     public static void main(String[] args) {
